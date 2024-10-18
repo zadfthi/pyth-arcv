04f4fc9fcd66ad50554ae6d58cdb3a1998827739
zadfthi <zainaldinfathi@gmail.com>
2024-10-18 13:51:29 +0700

    main

---
+++ combinestr.py
@@ -0,0 +1,9 @@
+text1 = 'hello'
+text2 = 'cuuuuy'
+num = 4
+num2 = '3'
+print(text1 + str(num))
+print(text2 * int(num2))
+print(text2 * num)
+print(text1 + '', text2)
+
---
+++ strmanipul.py
@@ -0,0 +1,12 @@
+name = 'test tast'
+print(name.replace('test', 'jasjus'))
+
+# print (name[0:1]) # Cut off A string
+
+# register = input('Masukkan Nama : ').upper() # All Capital
+# register1 = input('Masukkan Nama : ').lower() # All Lower
+# register2 = input('Masukkan Nama : ').capitalize() # Only First Word is Capitalized
+# register3 = input('Masukkan Nama : ').title() # First word is capita -e.g Test Tast
+
+# print(register, register1, register2, register3)
+
